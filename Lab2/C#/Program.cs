a959b7f9eea16f1dc094b72021bf3a2236484748
tim <tim.donin.dev@gmail.com>
2025-09-22 19:38:08 +0300

    Lab2: done

---
+++ Lab2/C#/Program.cs
@@ -0,0 +1,81 @@
+﻿class Program
+{
+/*
+2.1. Використовуючи генератор псевдовипадкових послідовностей чисел задати елементи матриці (матриць)
+в діапазоні -50…+49. Задано дві матриці A(n,n) і B(n,n) n=4. 
+Розробити програму, яка будує матрицю X(n,n) множенням елементів кожного рядка першої матриці 
+на найменше із значень елементів відповідного рядка другої матриці.
+*/
+
+    static void Task1()
+    {
+        Random rnd = new Random();
+        int n = 4;
+        int[,] A = new int[n, n];
+        int[,] B = new int[n, n];
+        int[,] X = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                A[i, j] = rnd.Next(-50, 50);
+                B[i, j] = rnd.Next(-50, 50);
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            int minB = B[i, 0];
+            for (int j = 1; j < n; j++)
+                if (B[i, j] < minB) minB = B[i, j];
+            for (int j = 0; j < n; j++)
+                X[i, j] = A[i, j] * minB;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                Console.Write(X[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+
+    /*
+2.2. Дано зубчастий масив з n рядків, у рядках по m[j (j=1..n) елементів. 
+Для кожного стовпця підрахувати суму парних додатних елементів і записати дані в новий масив.
+*/
+
+    static void Task2() 
+    {
+        Random rnd = new Random();
+        int n = rnd.Next(3, 6);
+        int[][] jagged = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            int m = rnd.Next(3, 7);
+            jagged[i] = new int[m];
+            for (int j = 0; j < m; j++) jagged[i][j] = rnd.Next(-10, 11);
+        }
+
+        int maxCols = 0;
+        for (int i = 0; i < jagged.Length; i++)
+            if (jagged[i].Length > maxCols) maxCols = jagged[i].Length;
+
+        int[] result = new int[maxCols];
+        for (int col = 0; col < maxCols; col++)
+        {
+            int sum = 0;
+            for (int row = 0; row < jagged.Length; row++)
+                if (col < jagged[row].Length && jagged[row][col] > 0 && jagged[row][col] % 2 == 0)
+                    sum += jagged[row][col];
+            result[col] = sum;
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+
+    static void Main()
+    {
+        Task1();
+        Console.ReadLine();
+        Task2();
+    }
+}
---
+++ Lab2/Task1.1/main.py
@@ -0,0 +1,36 @@
+﻿
+'''
+Задано масив. Визначити кількість елементів масиву, що перевищують 
+середньоарифметичне всіх елементів масиву та вивести їх на екран. 
+'''
+def task1():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    avg = sum(arr) / n
+    res = [x for x in arr if x > avg]
+    print("Amount of elements: {}".format(len(res)))
+    print(*res)
+
+
+'''
+Глядацька зала має 10 рядів по 15 місць. 
+У двовимірному масиві зберігається інформація про продані місця: 
+якщо елемент масиву дорівнює 1, то місце вважається зайнятим, якщо 0 – вільним.
+Написати програму, що визначає, чи є вільні місця в обраному користувачем ряді.
+'''
+def task2():
+    import random
+    rows, cols = 10, 15
+    hall = [[random.randint(0, 1) for _ in range(cols)] for _ in range(rows)]
+    r = int(input()) - 1
+    if 0 in hall[r]:
+        print("Vacant spots are present")
+    else:
+        print("There're no vacant spots left :(")
+    for i in range(rows):
+        print(*(hall[i]))
+
+if __name__ == '__main__':
+    task1()
+    print("= Task2: ")
+    task2()
